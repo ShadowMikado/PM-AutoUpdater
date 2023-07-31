fef15e98b200a412d88ae44b5f039a71c43f545f
ShadowMikado <89030950+ShadowMikado@users.noreply.github.com>
2023-07-31 13:05:51 +0200

    Update UpdateTask.php

--- src/ShadowMikado/PocketMineAutoUpdater/Updater/UpdateTask.php
+++ src/ShadowMikado/PocketMineAutoUpdater/Updater/UpdateTask.php
@@ -2,34 +2,74 @@
 
 namespace ShadowMikado\PocketMineAutoUpdater\Updater;
 
-use LogLevel;
 use pocketmine\scheduler\AsyncTask;
+use pocketmine\utils\Internet;
 
 class UpdateTask extends AsyncTask
 {
-
-    private const TLS_KEY_AUTOUPDATER = "autoupdater";
+    private const TLS_KEY_AUTO_UPDATER = "autoupdater";
+    private string $error = "Unknown error";
 
     public function __construct(UpdateCheckerMixin $updater)
     {
-        $this->storeLocal(self::TLS_KEY_AUTOUPDATER, $updater);
+        $this->storeLocal(self::TLS_KEY_AUTO_UPDATER, $updater);
     }
 
     public function onRun(): void
     {
+        if ($this->isConnected()) {
+            $error = "";
+            $response = Internet::getURL("https://update.pmmp.io/api", 4, [], $error);
+            $this->error = $error;
+
+            if ($response !== null) {
+                $response = json_decode($response->getBody(), true);
+                if (is_array($response)) {
+                    if (isset($response["error"]) && is_string($response["error"])) {
+                        $this->error = $response["error"];
+                    } else {
+                        $mapper = new \JsonMapper();
+                        $mapper->bExceptionOnMissingData = true;
+                        $mapper->bEnforceMapType = false;
+                        try {
+                            /** @var UpdateInfoMixin $responseObj */
+                            $responseObj = $mapper->map($response, new UpdateInfoMixin());
+                            $this->setResult($responseObj);
+                        } catch (\JsonMapper_Exception $e) {
+                            $this->error = "Invalid JSON response data: " . $e->getMessage();
+                        }
+                    }
+                } else {
+                    $this->error = "Invalid response data";
+                }
+            }
+        } else {
+            $this->error = "No internet connection";
+        }
     }
 
     public function onCompletion(): void
     {
-        /** @var UpdateCheckerMixin2 $updater */
-        $updater = $this->fetchLocal(self::TLS_KEY_AUTOUPDATER);
-        if ($updater->isConnected() == true) {
-            $updater->checkUpdate();
-            $updater->downloadUpdate();
-            $this->cancelRun();
+        /** @var UpdateCheckerMixin $updater */
+        $updater = $this->fetchLocal(self::TLS_KEY_AUTO_UPDATER);
+        if ($this->hasResult()) {
+            /** @var UpdateInfoMixin $response */
+            $response = $this->getResult();
+            $updater->checkUpdateCallback($response);
         } else {
-            $updater->printConsoleMessage("The server isn't connected to the internet !", LogLevel::ALERT);
-            $this->cancelRun();
+            $updater->checkUpdateError($this->error);
         }
+    }
+
+    public function isConnected(): bool
+    {
+        $connected = @fsockopen("google.com", 443);
+        if ($connected) {
+            $is_conn = true;
+            fclose($connected);
+        } else {
+            $is_conn = false;
+        }
+        return $is_conn;
     }
 }
